ae0263eaf34434a04adcfc71805962ae1f91752b
Fabien Roy <froy@jstrategia.com>
2022-05-06 15:00:49 -0400

    fix: use rem instead of px (fixes android build)

--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,14 +1,14 @@
 const spacing = {
-  xxs: '.25rem', // 4 px
-  xs: '.5rem', // 8 px
-  s: '.75rem', // 12 px
-  m: '1rem', // 16 px
-  l: '1.5rem', // 24 px
-  xl: '2.25rem', // 36 px
+  xxs: '4px',
+  xs: '8px',
+  s: '12px',
+  m: '16px',
+  l: '24px',
+  xl: '36px',
 
   input: {
-    height: '2.5rem', // 40 px
-    minWidth: '7.5rem', // 120 px
+    height: '40px',
+    minWidth: '120px',
   },
 };
 
