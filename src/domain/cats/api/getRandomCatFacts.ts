605219524861c3417c1e72d79213c75e94a3339c
Fabien Roy <fabienroy28@gmail.com>
2022-08-25 19:53:45 -0400

    feat: use new API correctly app

--- src/domain/cats/api/getRandomCatFacts.ts
+++ src/domain/cats/api/getRandomCatFacts.ts
@@ -1,12 +1,10 @@
-// import { CAT_FACTS_BASE_URL } from 'react-native-dotenv';
+import { CAT_FACTS_BASE_URL } from 'react-native-dotenv';
 import axios from 'axios';
 import mockedCatFacts from '../mocks/catFacts';
 import getFeaturesEnv from '../../../config/features-env';
 import type { AnimalFact } from '../../../types/cats';
 
-// const endpoint = `${CAT_FACTS_BASE_URL}/facts/random`;
-// TODO: Fix react-native-dotenv (local, development, production)
-const endpoint = `http://localhost:3000/facts/random`;
+const endpoint = `${CAT_FACTS_BASE_URL}/facts/random`;
 const amount = 3;
 
 function getEndpoint() {
@@ -14,7 +12,6 @@ function getEndpoint() {
 }
 
 async function getRandomCatFactsFromEndpoint() {
-  console.log(getEndpoint());
   const response = await axios.get<readonly AnimalFact[]>(getEndpoint());
   return response.data;
 }
